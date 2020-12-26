edb7b4f0bb29bf0e4c974e048a6652dad5a753f9
SizemovPavel <SizemovPavel@gmail.com>
2020-12-26 13:09:49 +0300

    fix logic

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -5,16 +5,17 @@ const rules = () => 'What number is missing in the progression?';
 
 const getQuestionAndAnswer = () => {
   const firstElement = getRandomInt(1, 50);
+  const progressionStep = getRandomInt(2, 10);
   const arr = [firstElement];
   const steps = 9;
   let step = 1;
   for (step; step <= steps; step += 1) {
-    arr[step] = arr[step - 1] + 3;
+    arr[step] = arr[step - 1] + progressionStep;
   }
   const getRadnomElement = getRandomInt(2, 9);
   const correctAnswer = arr[getRadnomElement];
   arr.splice(getRadnomElement, 1, '..');
-  const question = `${arr}`;
+  const question = arr.join(' ');
   return [question, correctAnswer];
 };
 
